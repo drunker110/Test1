086fd6301044adf47db4157ad27cfa7c1cfdd0df
CCB <841306289@qq.com>
2021-06-28 14:25:25 +0800

    添加时钟

--- Test1/Form1.Designer.cs
+++ Test1/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Test1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uiContextMenuStrip1 = new Sunny.UI.UIContextMenuStrip();
+            this.uiAnalogMeter1 = new Sunny.UI.UIAnalogMeter();
+            this.SuspendLayout();
+            // 
+            // uiContextMenuStrip1
+            // 
+            this.uiContextMenuStrip1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiContextMenuStrip1.Name = "uiContextMenuStrip1";
+            this.uiContextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // uiAnalogMeter1
+            // 
+            this.uiAnalogMeter1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiAnalogMeter1.Location = new System.Drawing.Point(329, 123);
+            this.uiAnalogMeter1.MaxValue = 100D;
+            this.uiAnalogMeter1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAnalogMeter1.MinValue = 0D;
+            this.uiAnalogMeter1.Name = "uiAnalogMeter1";
+            this.uiAnalogMeter1.Renderer = null;
+            this.uiAnalogMeter1.Size = new System.Drawing.Size(180, 180);
+            this.uiAnalogMeter1.TabIndex = 1;
+            this.uiAnalogMeter1.Text = "uiAnalogMeter1";
+            this.uiAnalogMeter1.Value = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uiAnalogMeter1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Sunny.UI.UIContextMenuStrip uiContextMenuStrip1;
+        private Sunny.UI.UIAnalogMeter uiAnalogMeter1;
     }
 }
 
